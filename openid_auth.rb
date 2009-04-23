193ce8618095c03fa821245a54e242f24c39fe07
Sausheong Chang <saush@blowabove-lm.singapore.corp.yahoo.com>
2009-04-23 16:55:19 +0800

    tidied up openid auth

--- openid_auth.rb
+++ openid_auth.rb
@@ -4,22 +4,19 @@ REALM = 'http://localhost:4567'
 RETURN_TO = "#{REALM}/complete"
 
 get '/login' do
-  identifier = params[:openid_identifier]
-  checkid_request = openid_consumer.begin(identifier)
+  checkid_request = openid_consumer.begin(params[:openid_identifier])
   redirect checkid_request.redirect_url(REALM, RETURN_TO) if checkid_request.send_redirect?(REALM, RETURN_TO)
   checkid_request.html_markup(REALM, RETURN_TO)
 end
 
 get '/complete' do
   response = openid_consumer.complete(params, RETURN_TO)
-  return response.identity_url.inspect if response.status == OpenID::Consumer::SUCCESS
+  return 'You are logged in!' if response.status == OpenID::Consumer::SUCCESS
   'Could not log on with your OpenID'
 end
 
 def openid_consumer
-  if @consumer.nil?
-    store = OpenID::Store::Filesystem.new('auth/store')
-    @consumer = OpenID::Consumer.new(session, store)
-  end
+  @consumer = OpenID::Consumer.new(session, OpenID::Store::Filesystem.new('auth/store')) if @consumer.nil?
   return @consumer
 end
+
