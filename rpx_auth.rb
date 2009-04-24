b4e899a2185e30ac219a6ead31b65a662d000c16
Sausheong Chang <saush@blowabove-lm.singapore.corp.yahoo.com>
2009-04-24 13:16:18 +0800

    tidied up rpx_auth

--- rpx_auth.rb
+++ rpx_auth.rb
@@ -5,7 +5,7 @@ get '/login' do
 end
 
 def authenticate(token)
-  response = JSON.parse(RestClient.post 'https://rpxnow.com/api/v2/auth_info', :token => token, 'apiKey' => '30e2f26bb637982d6b236dd7f095d2b5ba897b3f', :format => 'json')
+  response = JSON.parse(RestClient.post 'https://rpxnow.com/api/v2/auth_info', :token => token, 'apiKey' => '<INSERT API KEY HERE>', :format => 'json')
   return true if response['stat'] == 'ok'
   return false
 end
